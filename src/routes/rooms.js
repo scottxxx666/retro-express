cf1339bb8336506db046ed9ad7ffb58e4b4fd2b2
Scott Kao <scottxxx666@gmail.com>
2019-10-03 17:11:10 +0800

    Add async wrap to handle promise rejection

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,13 +1,14 @@
 import router from './router';
+import asyncWrap from './async-wrap';
 
-router.post('/', async (req, res) => {
+router.post('/', asyncWrap(async (req, res) => {
   const data = await req.container.roomService.create(req.auth.data.id);
   res.json({ data });
-});
+}));
 
-router.get('/:roomId', async (req, res) => {
+router.get('/:roomId', asyncWrap(async (req, res) => {
   const data = await req.container.roomService.find(req.params.roomId);
   res.json({ data });
-});
+}));
 
 export default router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
 import router from './router';
+import asyncWrap from './async-wrap';
 
-router.post('/login', async (req, res) => {
+router.post('/login', asyncWrap(async (req, res) => {
   const data = await req.container.userService.login(req.body.openId, 'GOOGLE');
   res.json({ data });
-});
+}));
 
 export default router;
