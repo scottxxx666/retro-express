cadadce86f6807e588b85de1dd64920f23737b42
Scott Kao <scottxxx666@gmail.com>
2019-10-06 01:56:45 +0800

    Replace document client to dynamoose in user

---
+++ src/services/user/models/open-id.js
@@ -0,0 +1,17 @@
+import dynamoose from 'dynamoose';
+
+export default dynamoose.model('retrospectives', new dynamoose.Schema({
+  // Open id, prefix: OPEN-ID
+  pk: {
+    type:String,
+    hashKey: true,
+  },
+  // Platform
+  sk: {
+    type: String,
+    rangeKey: true,
+  },
+  userId: {
+    type: String,
+  },
+}));
--- src/services/user/repository.js
+++ src/services/user/repository.js
@@ -1,33 +1,16 @@
+import OpenId from './models/open-id';
+
 export default class Repository {
   _tableName = 'retrospectives';
   _pkPrefix = 'OPEN-ID-';
 
-  constructor(documentClient) {
-    this._client = documentClient;
-  }
-
   async getId(platform, openId) {
-    const params = {
-      TableName: this._tableName,
-      Key: {
-        'pk': this._pkPrefix + openId,
-        'sk': platform,
-      }
-    };
-    const data = await this._client.get(params).promise();
-    return data.hasOwnProperty('Item') ? data.Item.pk : null;
+    const result = await OpenId.get({ pk: this._pkPrefix + openId, sk: platform });
+    return result === undefined ? null : result.userId;
   }
 
   async create(platform, openId, id) {
-    const params = {
-      TableName: this._tableName,
-      Item: {
-        'pk': this._pkPrefix + openId,
-        'sk': platform,
-        'userId': id,
-      }
-    };
-    await this._client.put(params).promise();
+    await OpenId.create({ pk: this._pkPrefix + openId, sk: platform });
     return id;
   }
 }
